658270a789518e43fa294abcb8b26a542b9af724
JanKremser <github@nerdbrief.de>
2023-11-07 18:28:32 +0100

    scss preprocessor

--- src/pages/index/mod.js
+++ src/pages/index/mod.js
@@ -1,7 +1,17 @@
 import { createSSRApp } from 'vue';
+
 const util = await import(`${process.cwd()}/src/.util/index.js`);
 
+import githubIcon from "../../components/icons/github-icon.js";
+import instagramIcon from "../../components/icons/instagram-icon.js";
+import linkedinIcon from "../../components/icons/linkedin-icon.js";
+
 export default createSSRApp({
+    components: {
+        githubIcon,
+        instagramIcon,
+        linkedinIcon,
+    },
     data: () => ({ count: 1 }),
     template: util.readTemplate(import.meta.url),
 });
