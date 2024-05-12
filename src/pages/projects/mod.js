52f4814cfc69bd1f504f89f9b7d0ae3dd347ece2
JanKremser <github@nerdbrief.de>
2024-05-12 17:50:59 +0200

    add oh_my_nvm project

--- src/pages/projects/mod.js
+++ src/pages/projects/mod.js
@@ -4,8 +4,20 @@ import pageWrapper from "../../components/wrapper.js";
 
 let template = /* html */`
 <page-wrapper>
-    <article>
-        coming soon
+    <article 
+        v-for="(item, index) in projects"
+        :key="index"
+    >
+        <div class="grid-2">
+            <div class="img-box">
+                <img :src="item.image" style="width: 12rem;" \>
+            </div>
+            <div style="padding-left: 2rem;">
+                <h2>{{ item.name }}</h2>
+                <p>{{ item.description }}</p>
+                <a :href="item.link" role="button" class="outline">GitHub</a>
+            </div>
+        </div>
     </article>
 </page-wrapper>
 `
@@ -15,7 +27,14 @@ export default createSSRApp({
         pageWrapper
     },
     data: () => ({
-
+        projects: [
+            {
+                name: "oh my nvm!",
+                description: "“oh my nvm!” is an nvm wrapper for easier use of different Node.js versions. Check this out ->",
+                link: "https://github.com/JanKremser/npmv",
+                image: "/img/projects/oh_my_nvm.png",
+            }
+        ]
     }),
     template,
 });
